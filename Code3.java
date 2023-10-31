caf8016a7c25575f87ddadc2a033334249f5deae
Prathmesh Parit <90789154+prathmeshparit@users.noreply.github.com>
2023-10-31 13:02:57 +0530

    Add files via upload

---
+++ Code3.java
@@ -0,0 +1,10 @@
+public class Code3 {
+    // Which one is not a valid statement in Java?
+    public static void main(String args[]) {
+        double num=2.718;
+        double num=2._718;
+        double num=2.7_1_8;
+    }
+
+}
+//output : double num=2._718;

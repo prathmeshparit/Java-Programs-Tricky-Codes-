8637c17a99a39e421192ee922d937e0bbb1be463
Prathmesh Parit <90789154+prathmeshparit@users.noreply.github.com>
2023-10-31 13:33:48 +0530

    Add files via upload

---
+++ Code8.java
@@ -0,0 +1,9 @@
+public class Code8 {
+    public static void main(String argsp[]) {
+        for (int i = 0; i < 1; i++) {
+            System.out.print(i + ' ');
+        }
+    }
+}
+
+// output: 32

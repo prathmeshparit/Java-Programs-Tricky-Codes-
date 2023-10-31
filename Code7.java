8e46ccedcdd4f291a184109be411e88162447036
Prathmesh Parit <90789154+prathmeshparit@users.noreply.github.com>
2023-10-31 13:31:34 +0530

    Add files via upload

---
+++ Code7.java
@@ -0,0 +1,11 @@
+public class Code7 {
+    public static void main(String args[]) {
+        for (int i = 0; 0; i++) {
+            System.out.println("Hello World");
+        }
+    }
+}
+
+// output:incompatible types: int cannot be converted to boolean
+// for (int i = 0; 0; i++) {
+// ^

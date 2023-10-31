85abe7ad303efc529761ecf5b13287d75724211c
Prathmesh Parit <90789154+prathmeshparit@users.noreply.github.com>
2023-10-31 13:14:34 +0530

    Add files via upload

---
+++ Code5.java
@@ -0,0 +1,13 @@
+public class Code5 {
+    // What line in this code snippet will give compilation error?
+    public static void main(String args[]) {
+        double d1 = 5f;
+        double d2 = 5.0;
+        float f1 = 5f;
+        float f2 = 5.0;
+    }
+
+}
+
+// output:
+// float f2 = 5.0;
